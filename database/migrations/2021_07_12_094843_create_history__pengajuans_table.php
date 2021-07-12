95838c677ed8794911390dffa49b35798a3af278
Wahyu Setiawan <wahyusetiaone27@gmail.com>
2021-07-12 16:58:28 +0700

    Adding table to History pengajuan dan penawaran

---
+++ database/migrations/2021_07_12_094843_create_history__pengajuans_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoryPengajuansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('history_pengajuans', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('kode_client')->default(0);
+            $table->string('nama_proyek', 100);
+            $table->text('diskripsi_proyek');
+            $table->string('alamat', 255);
+            $table->text('path');
+            $table->boolean('multipath')->default(false);
+            $table->boolean('offline')->default(false);
+            $table->bigInteger('range_min')->nullable();
+            $table->bigInteger('range_max')->nullable();
+            $table->date('deadline')->nullable();
+            $table->string('kode_status', 4)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('history__pengajuans');
+    }
+}
---
+++ database/migrations/2021_07_12_094907_create_history__penawarans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoryPenawaransTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('history_penawarans', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('kode_pin')->default(0);
+            $table->integer('keuntungan');
+            $table->bigInteger('harga_total');
+            $table->string('kode_status',4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('history__penawarans');
+    }
+}
